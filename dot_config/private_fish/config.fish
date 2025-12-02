f6181a414e67b4f1cb998d42aecc0f8aac0f0929
Rob Hand <146272+sinon@users.noreply.github.com>
2025-12-02 19:54:11 +0000

    cleanup fish conf

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,14 +1,32 @@
+set -gx fish_greeting ''
+
+set -gx HOMEBREW_BAT 1
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+set -gx HOMEBREW_BUILD_FROM_SOURCE 0
+set -gx HOMEBREW_NO_EMOJI 1
+set -gx HOMEBREW_NO_ANALYTICS 1
+
+set -gx RIPGREP_CONFIG_PATH ~/.config/.ripgreprc
+
+zoxide init fish | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    atuin init fish | source
+    mise activate fish | source
+    function atuin_lazy --on-event fish_prompt
+         atuin init fish | source
+         functions -e atuin_lazy
+    end
+    starship init fish | source
+else
+    mise activate fish --shims | source
 end
 
-starship init fish | source
-
-# Created by `pipx` on 2024-04-12 09:27:03
-set PATH $PATH /Users/Rob.Hand/.local/bin
-set RIPGREP_CONFIG_PATH $RIPGREP_CONFIG_PATH ~/.config/.ripgreprc
-zoxide init fish | source
+set -gx PATH  \
+    $HOME/.cargo/bin \
+    $HOME/bin \
+    $PATH \
+    $HOME/.local/bin \
+    /usr/local/bin
 
 abbr -a gco "git checkout"
 abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
