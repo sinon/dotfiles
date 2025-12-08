048a3ea889a01dbd299b8f400b06f03512500c99
Rob Hand <146272+sinon@users.noreply.github.com>
2025-12-08 11:53:49 +0000

    Adopt chezmoi (#2)
    * chezmoi
    
    * zed changes
    
    * Add Brewfiles
    
    * merge
    
    * fix readme rendering
    
    * drop omf, update gitconfig for non gpg
    
    * add rg conf
    
    * cleanup fish conf
    
    * remove default samples from ghostty config
    
    * add pip.conf
    
    * update ghostty config
    
    * update atuin config
    
    * fix README rendering
    
    * dont show stashed icon
    
    * switch default model
    
    * use Catppuccin Powerline preset for starship
    
    * fix whitespace

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,38 @@
+set -gx fish_greeting ''
+
+set -gx HOMEBREW_BAT 1
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+set -gx HOMEBREW_BUILD_FROM_SOURCE 0
+set -gx HOMEBREW_NO_EMOJI 1
+set -gx HOMEBREW_NO_ANALYTICS 1
+
+set -gx RIPGREP_CONFIG_PATH ~/.config/.ripgreprc
+
+zoxide init fish | source
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    mise activate fish | source
+    function atuin_lazy --on-event fish_prompt
+         atuin init fish | source
+         functions -e atuin_lazy
+    end
+    starship init fish | source
+else
+    mise activate fish --shims | source
+end
+
+set -gx PATH  \
+    $HOME/.cargo/bin \
+    $HOME/bin \
+    $PATH \
+    $HOME/.local/bin \
+    /usr/local/bin
+
+abbr -a gco "git checkout"
+abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
+if command -v eza > /dev/null
+    abbr -a ls 'eza --all --long --group --group-directories-first --icons --header --time-style long-iso --hyperlink'
+else
+    abbr -a l 'ls'
+    abbr -a ls 'ls -la'
+end
